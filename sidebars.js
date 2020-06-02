6187e825795adf7bd47177e0d8a49b832f59ebc6
Anoop B <16367697+anoop-b@users.noreply.github.com>
2020-06-02 19:09:16 +0530

    bip-0009 sidebar entry

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    BIPS: ['doc0', 'bip-0001', 'bip-0002','bip-0008'],
+    BIPS: ['doc0', 'bip-0001', 'bip-0002','bip-0008','bip-0009'],
     Contribute: ['contribute'],
   },
 };
